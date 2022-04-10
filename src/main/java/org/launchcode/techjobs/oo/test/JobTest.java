a55ec9a3bf7b137318265fac5b57e9faf3bd82ba
Lisa Ulricksen <lisa.joshua1.9@gmail.com>
2022-04-10 14:12:07 -0500

    completed Unit Testing Job Class

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,48 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.CoreCompetency;
+import org.launchcode.techjobs.oo.Employer;
+import org.launchcode.techjobs.oo.Job;
+import org.launchcode.techjobs.oo.Location;
+import org.launchcode.techjobs.oo.PositionType;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    Job emptyJob1;
+    Job emptyJob2;
+    Job completeJobTest1;
+    Job completeJobTest2;
+
+    @Before
+        public void createJobObject(){
+         emptyJob1 = new Job();
+         emptyJob2 = new Job();
+         completeJobTest1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence") );
+         completeJobTest2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    }
+
+    @Test
+    public void testSettingJobId(){
+        assertFalse(emptyJob1.equals(emptyJob2));
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        assertTrue(completeJobTest1 != null);
+    }
+
+    @Test
+    public void testJobsForEquality(){
+        assertFalse(completeJobTest1.equals(completeJobTest2));
+    }
 
 }
